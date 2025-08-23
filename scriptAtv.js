17810a6f13e7ca04b5eb0591acd7472baab1571a
jvocx1 <jvocx.dev@gmail.com>
2025-08-23 11:05:32 -0300

    Add files via upload

---
+++ scriptAtv.js
@@ -0,0 +1,13 @@
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let action = document.getElementById("action");
+
+
+action.addEventListener("click", function submitLogin (e) {
+    e.preventDefault()
+  if (email.value === "abc123@gmail.com" && password.value === "123456") {
+    alert("acesso autorizado!")
+} else {
+    alert("Verifique o login e a senha e tente novamente!")
+}  
+})
