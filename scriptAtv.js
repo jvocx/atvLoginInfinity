233f03ca2970f57821dc0b65f275d6b733e2b90f
jvocx <jvocx.dev@gmail.com>
2025-08-29 15:30:03 -0300

    Add files via upload

--- scriptAtv.js
+++ scriptAtv.js
@@ -1,11 +1,14 @@
-let email = document.getElementById("email");
-let password = document.getElementById("password");
+let email = document.getElementById("emailLogin");
+let emailCadastro = document.getElementById("email");
+let password = document.getElementById("passwordLogin");
+let passwordCadastro = document.getElementById("passwordCadastro");
 let action = document.getElementById("action");
+
 
 
 action.addEventListener("click", function submitLogin (e) {
     e.preventDefault()
-  if (email.value === " " && password.value === "123456") {
+  if (email.value === emailCadastro && password.value === "123456") {
     alert("acesso autorizado!")
 } else {
     alert("Verifique o login e a senha e tente novamente!")
